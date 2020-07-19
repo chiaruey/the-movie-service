29ec868b7a76b210eb0a3bf49253be5cae2062e8
chiaruey <chiaruey@gmail.com>
2020-07-19 12:16:19 -0700

    add cross origin

--- src/main/java/com/ruey/movie/search/controller/MovieSearchService.java
+++ src/main/java/com/ruey/movie/search/controller/MovieSearchService.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -15,6 +16,7 @@ import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/movie-search")
+@CrossOrigin
 public class MovieSearchService {
 
 	private static final String PATH = "/search/movie?query=";

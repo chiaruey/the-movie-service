b2e0f82b469291dc09fc8d8523d4d6dd8f6455d3
chiaruey <chiaruey@gmail.com>
2020-07-24 14:12:11 -0700

    remove Eureka and add poster path

--- src/main/java/com/ruey/movie/TheMovieServiceApplication.java
+++ src/main/java/com/ruey/movie/TheMovieServiceApplication.java
@@ -2,7 +2,6 @@ package com.ruey.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,7 +11,6 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableSwagger2
 public class TheMovieServiceApplication {
 
--- src/main/java/com/ruey/movie/search/model/Movie.java
+++ src/main/java/com/ruey/movie/search/model/Movie.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Movie {
 	private Integer id;
 	private String title;
+	private String original_title;
 	private String release_date;
 	private String overview;
 	private String vote_average;
@@ -12,8 +13,8 @@ public class Movie {
 	private List<Integer> genre_ids;
 	private String popularity;
 	private String original_language;
-	
-
+	private String poster_path;
+	private String backdrop_path;
 
 	public Integer getId() {
 		return id;
@@ -79,5 +80,30 @@ public class Movie {
 		this.vote_average = vote_average;
 	}
 
+	public String getPoster_path() {
+		return poster_path;
+	}
+
+	public void setPoster_path(String poster_path) {
+		this.poster_path = poster_path;
+	}
+
+	public String getBackdrop_path() {
+		return backdrop_path;
+	}
+
+	public void setBackdrop_path(String backdrop_path) {
+		this.backdrop_path = backdrop_path;
+	}
+
+	public String getOriginal_title() {
+		return original_title;
+	}
+
+	public void setOriginal_title(String original_title) {
+		this.original_title = original_title;
+	}
+
+	
 
 }

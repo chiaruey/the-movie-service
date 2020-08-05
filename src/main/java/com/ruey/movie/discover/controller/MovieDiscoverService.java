1e3432ef69a855acefeca26bf42b03d276ad0c80
chiaruey <chiaruey@gmail.com>
2020-08-05 10:14:47 -0700

    add cross origin to all services

--- src/main/java/com/ruey/movie/discover/controller/MovieDiscoverService.java
+++ src/main/java/com/ruey/movie/discover/controller/MovieDiscoverService.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,7 @@ import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/movie-discover")
+@CrossOrigin
 public class MovieDiscoverService {
 
 	private String POPULAR_PATH = "/discover/movie?sort_by=popularity.desc";
